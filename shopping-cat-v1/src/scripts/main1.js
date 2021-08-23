d48f0d18d4c6c55ae3bb78485af41e5d1fa4aa86
john <t69685186@gmail.com>
2021-08-23 23:16:19 +0800

    購物車

---
+++ shopping-cat-v1/src/scripts/main1.js
@@ -0,0 +1,75 @@
+// import "bootstrap/dist/css/bootstrap.css"
+// import "@fortawesome/fontawesome-free/css/all.css"
+// // 實作寫在這裡！
+
+// import axios from "axios"
+
+// function addToCart({ id }) {
+//     console.log(`把編號${id}`)
+// }
+// function add(cats) {
+//    const btns = document.querySelectorAll(".card .btn")
+//    btns.forEach((btn,i) => {
+//     btn.addEventListener("click",(e)=>{
+//         //    const cc = e.currentTarget.parentElement.parentElement
+//         //    const ppp = cc.dataset.ppp
+//            addToCart(cats[i])
+//        })
+//    })
+// }
+
+
+// document.addEventlistener("DOMcontentloaded",() => {
+
+// const ss = "https://raw.githubusercontent.com/5xTraining/shopping-cat-v1/main/data/cats.json"
+
+// const aa  = document.getElementById('items');
+// aa.insertAdjacentHTML('beforeend', c);
+// aa.insertAdjacentHTML('beforeend', c);
+// aa.insertAdjacentHTML('beforeend', c);
+// aa.insertAdjacentHTML('beforeend', c);
+// function clearCatCard({name},{id},{price}) {
+// const cat  = `<div class="items row">
+//             <div class="col-sm-2">
+//             <div class="card" data-product-id="624946E">
+//                 <img src="images/cat001.jpg" class="card-img-top" alt="" />
+//                 <div class="card-body">
+//                 <h5 class="title card-title fs-6 fw-light">${name}</h5>
+//                 <p class="price">$${price}</p>
+//                 <button class="btn btn-sm btn-warning fw-light">
+//                     <i class="fas fa-cat"></i>
+//                 </button>
+//                 </div>
+//             </div>
+//             </div>`
+//     return cat
+// }
+// clearCatCard()
+// axios
+//   .get(end)
+//   .then(function({data}) {
+//    data.forEach(element => {
+//        let cat = clearCatCard(data)
+//        {aa.insertAdjacentHTML('beforeend', c);}
+//    });
+//     add(data)
+//   })
+//   .catch((err)=> {
+//     console.log(err)
+//   })
+// })
+// function csshop({name, price, number}){
+//     const hoop = `<tr>
+//                  <td>老大</td>
+//                  <td><input type="number" class="quantity" value="1" /></td>
+//                  <td>$20</td>
+//                  <td>$20</td>
+//                  <td>
+//                  <button class="remove-item-btn btn btn-danger btn-sm">
+//                     <i class="fas fa-trash-alt"></i>
+//                  </button>
+//                  </td>
+//                  </tr>
+//                  <tr>`
+// return cshoop
+// }
