24ae36c3690b1ce30ab5a629b7348b43175214c1
john <t69685186@gmail.com>
2021-08-24 22:08:31 +0800

    react

---
+++ 02-study/01_helloworld/app.js
@@ -0,0 +1,23 @@
+// TODO: 1 start
+/*
+ReactDOM.render(
+  <h1>Hello, world!!</h1>,
+  document.getElementById('app')
+);
+// */
+// TODO: 1 end
+ReactDOM.render(
+  <h1>00</h1>,
+  document.getElementById('app')
+)
+ReactDOM.render(
+  <h2>11111</h2>,
+  document.getElementById('bpp')
+)
+ReactDOM.render(
+  <div>
+  <h1>111z</h1>
+  <h2>11111</h2>
+  </div>,
+  document.getElementById('app')
+)
