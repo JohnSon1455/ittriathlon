ce2a2216fb4d2a7de44e5861ab7f68b25ee534d6
john <t69685186@gmail.com>
2021-10-13 22:01:26 +0800

    js 099 Solution

---
+++ leecode/js/099-Recover Binary Search Tree.js
@@ -0,0 +1,22 @@
+var recoverTree = function(root) {
+  // javascript can't pass value by reference 
+  // so pass array object by reference
+  let first = [null];
+  let second = [null];
+  let prev = [null];
+  inorder(root, first, second, prev);
+  [first[0].val, second[0].val] = [second[0].val, first[0].val];
+};
+
+function inorder(root, first, second, prev) {
+  // Because inorder traverse BST, all values are sorted.
+  // Using inorder traversal to find two nodes that have prev.val > root.val
+  if (root === null) return;
+  inorder(root.left, first, second, prev);
+  if (prev[0] && prev[0].val > root.val) {
+      if (first[0] === null) first[0] = prev[0];
+      second[0] = root;
+  }
+  prev[0] = root;
+  inorder(root.right, first, second, prev);
+}
