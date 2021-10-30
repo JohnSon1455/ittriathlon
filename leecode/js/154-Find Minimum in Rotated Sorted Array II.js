b51228001339f1d7cc06a50cd6f99c5cfcaed742
john <t69685186@gmail.com>
2021-10-30 21:35:42 +0800

    js 154 Solution

---
+++ leecode/js/154-Find Minimum in Rotated Sorted Array II.js
@@ -0,0 +1,15 @@
+var findMin = function(nums) {
+  var left = 0;
+  var right = nums.length - 1;
+  while (left < right) {
+  var   mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else if (nums[mid] < nums[right]) {
+      right = mid;
+    } else {
+      right--;
+    }
+  }
+  return nums[left];
+};
